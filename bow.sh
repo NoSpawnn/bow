ad4d7cb1f8327d952352efc232c6b82495ee0205
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-07 01:33:58 +0000

    formatting/docs overhaul

--- bow.sh
+++ bow.sh
@@ -2,16 +2,34 @@
 
 DRY_RUN=0
 
+# ------------------------- #
+# General utility functions #
+# ------------------------- #
+
+#######################################
+# Spit out a formatted error message to stderr with a timestamp
+# Arguments:
+#   $1 - error message
+#######################################
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
 # ---------------------- #
 # Installation functions #
 # ---------------------- #
 
+#######################################
+# Install flatpaks based on IDs and scopes in a YAML file
+# Arguments:
+#   $1 - yaml file relative to this script to read from
+#######################################
 install_flatpaks() {
     local default_scope=$(yq '.flatpak.default_scope' < $1)
 
     if [[ ! "$default_scope" =~ ^(user|system)$ ]]; then
-        echo "Invalid default scope '$default_scope', must be one of: user, system"
-        exit 1
+      echo "Invalid default scope '$default_scope', must be one of: user, system"
+      exit 1
     fi
 
     local flatpaks_to_install_user=()
@@ -19,52 +37,52 @@ install_flatpaks() {
 
     function add_to_relevant_list() {
         if [[ "$2" == "user" ]]; then
-            flatpaks_to_install_user+=("$1")
+          flatpaks_to_install_user+=("$1")
         elif [[ "$2" == "system" ]]; then
-            flatpaks_to_install_system+=("$1")
+          flatpaks_to_install_system+=("$1")
         else
-            echo "Invalid scope '$2' for $1, must be one of: user, system"
-            exit 1
+          echo "Invalid scope '$2' for $1, must be one of: user, system"
+          exit 1
         fi
     }
 
     # map entries (specify an ID and scope key)
     local yqexpr_object_flatpaks='
-        .flatpak.present.[] |
-        select(tag == "!!map") |
-        [ .id, .scope ] |
-        join("|")
+      .flatpak.present.[] |
+      select(tag == "!!map") |
+      [ .id, .scope ] |
+      join("|")
     '
     while IFS='|' read -r fp_id fp_scope; do
-        if [[ -z "$fp_id" && ! -z "$fp_scope" ]]; then
-            echo "Defining only a 'scope' is invalid"
-            exit 1
-        fi
-        add_to_relevant_list "$fp_id" "$fp_scope"
+      if [[ -z "$fp_id" && ! -z "$fp_scope" ]]; then
+        echo "Defining only a 'scope' is invalid"
+        exit 1
+      fi
+      add_to_relevant_list "$fp_id" "$fp_scope"
     done < <(yq eval "$yqexpr_object_flatpaks" < "$1")
 
     # str entries
     local yqexpr_simple_flatpaks='
-        .flatpak.present.[] |
-        select(tag == "!!str")
+      .flatpak.present.[] |
+      select(tag == "!!str")
     '
     while read -r fp_id; do
-        local fp_scope="$default_scope"
-        add_to_relevant_list "$fp_id" "$fp_scope"
+      local fp_scope="$default_scope"
+      add_to_relevant_list "$fp_id" "$fp_scope"
     done < <(yq eval "$yqexpr_simple_flatpaks" < "$1")
 
     if [[ "$DRY_RUN" == 1 ]]; then
-        echo "Would install ${#flatpaks_to_install_system[@]} system flatpaks"
-        printf '    %s\n' ${flatpaks_to_install_system[@]}
-        echo "Would install ${#flatpaks_to_install_user[@]} user flatpaks"
-        printf '    %s\n' ${flatpaks_to_install_user[@]}
+      echo "Would install ${#flatpaks_to_install_system[@]} system flatpaks"
+      printf '    %s\n' ${flatpaks_to_install_system[@]}
+      echo "Would install ${#flatpaks_to_install_user[@]} user flatpaks"
+      printf '    %s\n' ${flatpaks_to_install_user[@]}
     else
-        for fp in "${flatpaks_to_install_system[@]}"; do
-            flatpak install --system "$fp"
-        done
-        for fp in "${flatpaks_to_install_user[@]}"; do
-            flatpak install --user "$fp"
-        done
+      for fp in "${flatpaks_to_install_system[@]}"; do
+        flatpak install --system "$fp"
+      done
+      for fp in "${flatpaks_to_install_user[@]}"; do
+        flatpak install --user "$fp"
+      done
     fi
 }
 
@@ -73,23 +91,29 @@ install_flatpaks() {
 # ---- #
 
 while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
-    case "$1" in
-        -d | --dry-run )
-            DRY_RUN=1
-            ;;
-    esac; shift; done
+  case "$1" in
+    -d | --dry-run ) DRY_RUN=1 ;;
+  esac
+  shift
+done
 if [[ "$1" == '--' ]]; then shift; fi
 
-readonly YAML_FILE="$1"
-if [[ ! -e "$YAML_FILE" ]]; then
-    echo "$YAML_FILE does not exist"
-    exit 1
-elif [[ ! -f "$YAML_FILE" ]]; then
-    echo "$YAML_FILE" is not a file
-    exit 1
-elif ! yq eval "$YAML_FILE" 1>/dev/null; then
-    # yq will print its own error message
-    exit 1
-fi
+main() {
+  local yaml_file="$1"
 
-install_flatpaks $YAML_FILE
+  if [[ ! -e "$yaml_file" ]]; then
+    err "$yaml_file does not exist"
+    exit 1
+  elif [[ ! -f "$yaml_file" ]]; then
+    err "$yaml_file" is not a file
+    exit 1
+  elif ! yq eval "$yaml_file" &>/dev/null; then
+    # there's gotta be a nicer way to do this? where's my if let...
+    err "$(yq eval \"$yaml_file\")"
+    exit 1
+  fi
+
+  install_flatpaks $yaml_file
+}
+
+main "$@"
